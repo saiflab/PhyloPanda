0e8acfe7c2c7b999345b621d690c3f5ea2a29ed6
Ahmed Saif <141224368+saiflab@users.noreply.github.com>
2024-09-03 23:23:47 +0600

    Update phylogenetic_tree_builder.py

--- phylogenetic_tree_builder.py
+++ phylogenetic_tree_builder.py
@@ -1,51 +1,50 @@
-
 import streamlit as st
-from Bio import Phylo, AlignIO
-from Bio.Align.Applications import ClustalOmegaCommandline
+from Bio import Phylo
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor, DistanceCalculator
+from Bio import AlignIO, SeqIO
 from io import StringIO
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Title and description
-st.title("Phylogenetic Tree Builder")
-st.write("Upload a DNA FASTA file and generate a phylogenetic tree.")
+st.title("Simple Phylogenetic Tree Builder")
+st.write("Upload a DNA FASTA file to generate a phylogenetic tree.")
 st.write("Created by Ahmed Saif")
 
 # Upload FASTA file
 uploaded_file = st.file_uploader("Choose a FASTA file", type="fasta")
 
 if uploaded_file is not None:
-    # Save the uploaded file content to a string
-    fasta_content = uploaded_file.read().decode("utf-8")
-    
-    # Write the content to a temporary file
-    with open("sequences.fasta", "w") as f:
-        f.write(fasta_content)
-    
-    # Run Clustal Omega to align sequences (you need to have Clustal Omega installed)
-    clustalomega_cline = ClustalOmegaCommandline(infile="sequences.fasta", outfile="aligned.aln", verbose=True, auto=True)
-    clustalomega_cline()
-    
-    # Read the aligned sequences
-    aligned = AlignIO.read("aligned.aln", "clustal")
-    
-    # Create a phylogenetic tree using the aligned sequences
-    with open("aligned.aln", "r") as aln_file:
-        alignment = AlignIO.read(aln_file, "clustal")
-    
-    # Generate the tree from alignment
-    from Bio.Phylo.TreeConstruction import DistanceTreeConstructor, DistanceCalculator
-    calculator = DistanceCalculator('identity')
-    dm = calculator.get_distance(alignment)
-    
-    constructor = DistanceTreeConstructor()
-    tree = constructor.nj(dm)
-    
-    # Display the tree
-    Phylo.draw(tree)
-    st.pyplot(plt)
-    
-    # Optionally download the tree
-    st.download_button(label="Download Newick Tree", data=tree.format("newick"), file_name="phylogenetic_tree.newick", mime="text/plain")
+    sequences = list(SeqIO.parse(uploaded_file, "fasta"))
+
+    if len(sequences) > 1:
+        # Simulate sequence alignment for this simple implementation
+        alignment = AlignIO.MultipleSeqAlignment(sequences)
+
+        # Calculate the distance matrix based on sequence identity
+        calculator = DistanceCalculator('identity')
+        distance_matrix = calculator.get_distance(alignment)
+
+        # Build a phylogenetic tree using the Neighbor-Joining method
+        constructor = DistanceTreeConstructor()
+        tree = constructor.nj(distance_matrix)
+
+        # Display the tree using matplotlib
+        fig = plt.figure(figsize=(10, 6))
+        Phylo.draw(tree, do_show=False)
+        st.pyplot(fig)
+
+        # Download option for the tree in Newick format
+        newick_str = StringIO()
+        Phylo.write(tree, newick_str, "newick")
+        st.download_button(label="Download Tree in Newick Format", 
+                           data=newick_str.getvalue(), 
+                           file_name="phylogenetic_tree.newick",
+                           mime="text/plain")
+    else:
+        st.error("Please upload a FASTA file with more than one sequence.")
+else:
+    st.info("Waiting for a FASTA file upload...")
 
 # Footer
 st.write("---")
